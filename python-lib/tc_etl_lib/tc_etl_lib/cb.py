3a31f13ce8c00d61872e4570ff20c194d6d781da
kevinacuna <kevinacunakram@gmail.com>
2024-02-02 11:01:29 +0100

    Update python-lib/tc_etl_lib/tc_etl_lib/cb.py
    Co-authored-by: Fermín Galán Márquez <fgalan@users.noreply.github.com>

--- python-lib/tc_etl_lib/tc_etl_lib/cb.py
+++ python-lib/tc_etl_lib/tc_etl_lib/cb.py
@@ -99,7 +99,6 @@ class cbManager:
             self.session.close()
         except Exception:  # pylint: disable=broad-except
             logger.error(f'Error closing session with endpoint: {self.endpoint}')
-            pass
 
     def delete_entities(self, *, service: str = None, subservice: str = None, auth: authManager = None, limit: int = 100, type: str = None, q: str = None, mq: str = None, georel: str = None, geometry: str = None, coords: str = None, id: str = None, options_get: list = [], options_send: list = []):
         """Delete data from context broker
