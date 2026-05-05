1223967198cebe123d3526d784521ddc7380c795
oriana <orianaromero221293@gmail.com>
2026-05-05 00:05:07 -0500

    update version

--- python-lib/tc_etl_lib/setup.py
+++ python-lib/tc_etl_lib/setup.py
@@ -23,7 +23,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.18.0.1'
+VERSION = '0.18.1'
 PACKAGE_NAME = 'tc_etl_lib'
 AUTHOR = ''
 AUTHOR_EMAIL = ''
