8fdcdcd04a66b4548d17069872639903a6c3be93
oriana <orianaromero221293@gmail.com>
2026-05-06 11:30:03 -0500

    Step: 0.19.0.post -> 0.20.0

--- python-lib/tc_etl_lib/setup.py
+++ python-lib/tc_etl_lib/setup.py
@@ -23,7 +23,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.19.0.post'
+VERSION = '0.20.0'
 PACKAGE_NAME = 'tc_etl_lib'
 AUTHOR = ''
 AUTHOR_EMAIL = ''
